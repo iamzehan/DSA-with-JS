484048d7e9e409c510279a69763ef87cc954f24e
Zehan <zehan.kareem@gmail.com>
2025-10-17 04:46:26 +0600

    Added some comments

---
+++ Project-BinarySearchTrees.js
@@ -0,0 +1,6 @@
+// We are about to create  a binary search tree
+// There is a root node where it begins
+// There are parent-child nodes
+// nodes with no children are called leaf nodes
+// We are also aware of the BFS and DFS at this point
+// It's supposed to be easy now, so let's go!
