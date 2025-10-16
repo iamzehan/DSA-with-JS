b3266bbe169b02d1c960445b9632102f6f5ad841
Zehan <zehan.kareem@gmail.com>
2025-10-17 05:01:15 +0600

    Made queue implementation a module

---
+++ utils/queue.js
@@ -0,0 +1,30 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    return this.isEmpty() ? null : this.items.shift();
+  }
+
+  peek() {
+    return this.isEmpty() ? "Queue is empty" : this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.join(" -> "));
+  }
+}
+export default Queue;
