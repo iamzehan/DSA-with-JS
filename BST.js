e338dc0480e3f7638c953800bf4334bb06730b18
Zehan <zehan.kareem@gmail.com>
2025-10-17 04:46:09 +0600

    Experiment file for BST

---
+++ BST.js
@@ -0,0 +1,5 @@
+// Balanced Binary Search Tree (BST)
+// The difference between the heights of the left and right subtrees are gonna be at most one
+
+
+// The idea is to build a tree from a sorted array
