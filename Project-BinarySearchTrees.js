2a3a7955c535316f9cd9ab8e3b9613c294c68bd2
Zehan <zehan.kareem@gmail.com>
2025-10-17 07:36:17 +0600

    Build tree functionality created

--- Project-BinarySearchTrees.js
+++ Project-BinarySearchTrees.js
@@ -4,3 +4,39 @@
 // nodes with no children are called leaf nodes
 // We are also aware of the BFS and DFS at this point
 // It's supposed to be easy now, so let's go!
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+class Tree {
+  #array;
+  constructor(array) {
+    // remove duplicates and sort the array
+    this.#array = [...new Set(array)].sort((a, b) => a - b);
+    this.root = this.#buildTree(this.#array);
+  }
+
+  #buildTree = (array) => {
+    // base case
+    if (array.length === 0) return null;
+
+    // middle element is the root
+    let mid = Math.floor(array.length / 2);
+    let root = new Node(array[mid]);
+
+    // from start to the previous element of the root are left nodes
+    root.left = this.#buildTree(array.slice(0, mid));
+    // from the next element of the root are considered right nodes
+    root.right = this.#buildTree(array.slice(mid + 1));
+
+    return root;
+  };
+}
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+console.log(JSON.stringify(tree, null, 2));
