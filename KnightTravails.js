5fcfb32e7253939d1cbf72bdb2900d5969126807
Zehan <zehan.kareem@gmail.com>
2025-10-18 06:01:24 +0600

    Behold the Knight travails!

--- KnightTravails.js
+++ KnightTravails.js
@@ -50,6 +50,10 @@ are -
 - chessBoard[i+1][j-2] -> one step forward two steps to the left
 - chessBoard[i+2][j-1] -> two step to the side one step to the left
 - chessBoard[i+2][j+1] -> two step to the side one step to the right
+- chessBoard[i-1][j+2] → one step backward, two right
+- chessBoard[i-1][j-2] → one step backward, two left
+- chessBoard[i-2][j+1] → two left, one forward
+- chessBoard[i-2][j-1] → two left, one backward
 ------------------------------------------------------------------------------------
 
 Notes: 
@@ -69,3 +73,64 @@ Task:
 - build a knightMoves function that shows shortest possible ways to get from one square
 to another by outputting all squares the knight will stop on along the way.
 */
+
+import Queue from "./utils/queue.js";
+
+// let's create a helper function that will return an array of possible next moves
+function getKnightMoves([i, j]) {
+  const moves = [
+    [i + 2, j + 1],
+    [i + 2, j - 1],
+    [i - 2, j + 1],
+    [i - 2, j - 1],
+    [i + 1, j + 2],
+    [i + 1, j - 2],
+    [i - 1, j + 2],
+    [i - 1, j - 2],
+  ];
+  return moves.filter(([p, q]) => p >= 0 && p < 8 && q >= 0 && q < 8);
+}
+
+// let's create another helper function that tells us whether a target is matched to the next move
+function isMatched(current, target) {
+  return current[0] === target[0] && current[1] === target[1];
+}
+
+function knightMoves(start, target) {
+  // if the target is the same as the start
+  if (isMatched(start, target)) return { moves: 0, path: [start] };
+  // create a queue
+  const Q = new Queue();
+  // initialize the position and the path
+  Q.enqueue({ pos: start, path: [start] });
+  // the visited path 
+  const visited = new Set([start.toString()]);
+
+  while (!Q.isEmpty()) {
+    // pick the first path and position
+    const { pos, path } = Q.dequeue();
+    // iterate through it's next possible moves
+    for (const next of getKnightMoves(pos)) {
+      // if the next path wasn't visited before
+      if (!visited.has(next.toString())) {
+        // add the next path to existing path and call it a new path
+        const newPath = [...path, next];
+        // if the next move matches to the current
+        if (isMatched(next, target)) {
+          // return number of all moves and the latest updated path
+          const result = { moves: newPath.length - 1, path: newPath };
+          return `You got there in ${result.moves} moves.\nYour path is >>\n${result.path.join(" >>-> ")}`
+        }
+        // add the next path to visited
+        visited.add(next.toString());
+        // push the next position and path to the queue
+        Q.enqueue({ pos: next, path: newPath });
+      }
+    }
+  }
+  // might not happen ever
+  return { moves: -1, path: [] }; 
+}
+
+// let's call the knightMoves
+console.log(knightMoves([3, 3], [4, 3]));
